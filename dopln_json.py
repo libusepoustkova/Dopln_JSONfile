e36613521ba5f4b28b2bd16afcd3e31c6c343b08
Líba Poustková <libuse.poustkova@seznam.cz>
2019-04-27 23:47:48 +0200

    Verze 1 doplneni odkazu JVF do JSON souoru ontologie

---
+++ dopln_json.py
@@ -0,0 +1,67 @@
+
+import json
+#with open('ontologie_v9.json') as handle:
+#    dictdump = json.loads(handle.read(),encoding='utf8')
+#print(dictdump)
+
+
+def nacti_JSON():
+#fnction reads JSON file
+    try:
+       with open('ontologie_definiceV2.json', encoding='utf8') as ontoJsonFile:
+            ontoJsonStr = ontoJsonFile.read()
+            #print(json.dumps(ontoJsonStr)) #, ensure_ascii=False, indent=2))
+            ontoJson_data = json.loads(ontoJsonStr)
+            #print(ontoJson_data)
+            return ontoJsonStr
+
+    except FileNotFoundError:
+        print("Nenalezeno")
+
+def nacti_doplnovana_data(listDopln):
+#function reads text file to list of list
+    try:
+        with open('TextFile1.txt', encoding='ansi') as doplnFile:
+           
+            for radek in doplnFile:
+                radek = radek.rstrip("\n")
+                listDopln.append(radek.split(";"))
+
+            #print(listDopln)
+            return listDopln
+    except FileNotFoundError:
+        print("Nenalezeno")
+
+def preved_na_slovnik(listDopln):
+#function convert my special list to dictionary,the first item in the list is the key, the other is the value in the list
+    slDopln = {}   
+    for polozka in listDopln:
+        slDopln[polozka[0]] = polozka[1:]
+    return slDopln
+
+def dopln_JVFK_do_retezce(JSONstr,slDopln):
+
+    novyJSON = JSONstr
+    for onto, dtm in slDopln.items():
+        maleonto = onto.lower()
+        malynovyJSON = novyJSON.lower()
+        #Onto = onto.capitalize()
+        if  maleonto in malynovyJSON:
+            #print("nalezeno: " + Onto)
+            poziceOnto = malynovyJSON.index(maleonto)
+            poziceJVFDTM = poziceOnto + len(onto) + 3
+            novyJSON = '{0}"JVF_DTM":{1},{2}'.format(novyJSON[0:poziceJVFDTM], dtm, novyJSON[poziceJVFDTM:])
+        else:
+            print("Nenalezeno: " + onto)
+    return novyJSON
+
+
+listDoplneni = []
+listDoplneni = nacti_doplnovana_data(listDoplneni)
+#Data = nacti_JSON()
+retezecJSON = nacti_JSON()
+
+slovnikDopl = {}
+slovnikDopln = preved_na_slovnik(listDoplneni)
+novyJSONstr=dopln_JVFK_do_retezce(retezecJSON, slovnikDopln)
+print(novyJSONstr)
